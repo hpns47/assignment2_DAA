7a032bf854b838c52d3c27986c6264c7b65eed58
hpns47 <dani.abd.20069@gmail.com>
2025-10-04 16:22:19 +0500

    counters and csv

---
+++ src/main/java/metrics/PerformanceTracker.java
@@ -0,0 +1,9 @@
+package metrics;
+
+public class PerformanceTracker {
+    public long comparisons = 0;
+    public long swaps = 0;
+    public long earlyTerminations = 0;
+    public long runtimeNanos = 0;
+    public long allocations = 0;
+}
